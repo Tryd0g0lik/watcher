9c291445c28868385c90d37b47eeed12cd5f1cb0
Tryd0g0lik <work80@mail.ru>
2024-01-25 06:48:35 +0700

    [01]: HOUR has been replaces. IT before is date.getHours, after the date.getUTCHours

--- src/frontend/src/functions.ts
+++ src/frontend/src/functions.ts
@@ -1,22 +1,18 @@
-export default function fun(): void {
+export default function fun(h: number): void {
   const root = document.getElementById("root");
   if (root === null) return;
 
   setInterval(() => {
     const date = new Date();
-    const T_SEC = 6 * date.getSeconds();
-    const T_MIN = 6 * (date.getMinutes() + (1 / 60) * date.getSeconds()); // Определяем угол для минут
-    const T_HOUR = 30 * (date.getHours() + (1 / 60) * date.getMinutes()); // Определяем угол для часов
+    const T_SEC = 6 * date.getUTCSeconds();
+    const T_MIN = 6 * (date.getUTCMinutes() + (1 / 60) * date.getSeconds()); // Определяем угол для минут
+    const T_HOUR = 30 * (date.getUTCHours() + h + (1 / 60) * date.getMinutes()); // Определяем угол для часов
 
-    // let span: HTMLSpanElement;
     const elem = <HTMLCollectionOf<HTMLSpanElement> | null>root.getElementsByClassName("indicator");
     if (elem !== null) {
-      // span = elem[0];
-      elem[0].style.transform = `rotate(${T_SEC}deg)`;
-      elem[1].style.transform = `rotate(${T_HOUR}deg)`;
-      elem[2].style.transform = `rotate(${T_MIN}deg)`;
+      elem[0].style.transform = `rotate(${T_SEC + 90}deg)`;
+      elem[1].style.transform = `rotate(${T_HOUR + 90}deg)`;
+      elem[2].style.transform = `rotate(${T_MIN + 90}deg)`;
     }
   }, 1000);
 }
-
-
