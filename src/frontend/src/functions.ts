b8289ffa48bfc8ad8a690bf4903aa218404f5e01
Tryd0g0lik <work80@mail.ru>
2024-01-25 06:15:02 +0700

    [01]: indicate for a T_MIN, T_HUR is moke

--- src/frontend/src/functions.ts
+++ src/frontend/src/functions.ts
@@ -5,18 +5,18 @@ export default function fun(): void {
   setInterval(() => {
     const date = new Date();
     const T_SEC = 6 * date.getSeconds();
-    // const T_MIN = 6 * (date.getMinutes() + (1 / 60) * date.getSeconds()); // Определяем угол для минут
-    // const T_HOUR = 30 * (date.getHours() + (1 / 60) * date.getMinutes()); // Определяем угол для часов
+    const T_MIN = 6 * (date.getMinutes() + (1 / 60) * date.getSeconds()); // Определяем угол для минут
+    const T_HOUR = 30 * (date.getHours() + (1 / 60) * date.getMinutes()); // Определяем угол для часов
 
-    let span: HTMLSpanElement;
+    // let span: HTMLSpanElement;
     const elem = <HTMLCollectionOf<HTMLSpanElement> | null>root.getElementsByClassName("indicator");
     if (elem !== null) {
-      span = elem[0];
-      span.style.transform = `rotate(${T_SEC}deg)`;
+      // span = elem[0];
+      elem[0].style.transform = `rotate(${T_SEC}deg)`;
+      elem[1].style.transform = `rotate(${T_HOUR}deg)`;
+      elem[2].style.transform = `rotate(${T_MIN}deg)`;
     }
   }, 1000);
 }
 
-// в web  видно отсчет секунды.Не получаю исменение угла.
-// может turn изменить на что то
-// или rotate заменить translate
+
