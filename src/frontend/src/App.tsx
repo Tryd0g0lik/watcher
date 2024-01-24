b12aee6bed933697a840fc56de88c197217b0244
Tryd0g0lik <work80@mail.ru>
2024-01-24 16:12:33 +0700

    [01]: Here is a first commit

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import FormFC from "./components/Forms/index.tsx";
-import WatcherFC from "./components/Watchers/index.tsx";
-
+import WatcherFC from "./components/Watcher/index.tsx";
 export default function AppFC(): React.JSX.Element {
   return (
     <>
---
+++ src/frontend/src/components/Watcher/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Watcher.css";
+
+export default function WatcherFC(): React.JSX.Element {
+  return (
+    <div className="watch">
+      <span className="hour-mark"></span>
+      <span className="hour-mark"></span>
+    </div>
+  );
+}
