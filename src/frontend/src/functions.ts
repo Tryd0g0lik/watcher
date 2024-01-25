2f2a395688257f621cb45a653ac829f26ab5abd1
Tryd0g0lik <work80@mail.ru>
2024-01-25 07:46:42 +0700

    [02]: function 'fun' was rewrited to the class 'Times'

--- src/frontend/src/functions.ts
+++ src/frontend/src/functions.ts
@@ -1,18 +1,30 @@
-export default function fun(h: number): void {
-  const root = document.getElementById("root");
-  if (root === null) return;
+export default class Times {
+  UTC: number;
+  root: Record<any, any>;
+  constructor(utc: number) {
+    this.UTC = utc;
+  }
 
-  setInterval(() => {
-    const date = new Date();
-    const T_SEC = 6 * date.getUTCSeconds();
-    const T_MIN = 6 * (date.getUTCMinutes() + (1 / 60) * date.getSeconds()); // Определяем угол для минут
-    const T_HOUR = 30 * (date.getUTCHours() + h + (1 / 60) * date.getMinutes()); // Определяем угол для часов
+  set boxTime(root: Record<any, any>) {
+    this.root = root;
+  }
 
-    const elem = <HTMLCollectionOf<HTMLSpanElement> | null>root.getElementsByClassName("indicator");
-    if (elem !== null) {
-      elem[0].style.transform = `rotate(${T_SEC + 90}deg)`;
-      elem[1].style.transform = `rotate(${T_HOUR + 90}deg)`;
-      elem[2].style.transform = `rotate(${T_MIN + 90}deg)`;
-    }
-  }, 1000);
+  timeZone(): void {
+    const root = this.root;
+    console.log("[ROOT]: ", this.root);
+    console.log("[ROOT]: ", root);
+    setInterval(() => {
+      const date = new Date();
+      const T_SEC = 6 * date.getUTCSeconds();
+      const T_MIN = 6 * (date.getUTCMinutes() + (1 / 60) * date.getSeconds()); // Определяем угол для минут
+      const T_HOUR = 30 * (date.getUTCHours() + this.UTC + (1 / 60) * date.getMinutes()); // Определяем угол для часов
+
+      const elem = <HTMLCollectionOf<HTMLSpanElement> | null>root.getElementsByClassName("indicator");
+      if (elem !== null) {
+        elem[0].style.transform = `rotate(${T_SEC + 90}deg)`;
+        elem[1].style.transform = `rotate(${T_HOUR + 90}deg)`;
+        elem[2].style.transform = `rotate(${T_MIN + 90}deg)`;
+      }
+    }, 1000);
+  }
 }
